99b9a85be4b2b48254f1658604c7e9b6a702e977
Mykhailo Hozhyi <gozhenky@gmail.com>
2024-11-02 19:03:49 +0200

    module7-lesson1

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTasks } from './redux/operations';
+
 export const App = () => {
-  return <div></div>;
+  const dispatch = useDispatch();
+  const { items, isLoading, error } = useSelector(state => state.tasks);
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+
+  return (
+    <div>
+      {isLoading && <p>Loading tasks...</p>}
+      {error && <p>{error}</p>}
+      <p>{items.length > 0 && JSON.stringify(items, null, 2)}</p>
+    </div>
+  );
 };
